c90ebff496d55d9e43a1b09588fb1f4af1f59930
dsypher2 <149084998+dsypher2@users.noreply.github.com>
2026-06-22 11:01:54 -0500

    Update WorldQuestTracker_ZoneMap.lua

--- WorldQuestTracker_ZoneMap.lua
+++ WorldQuestTracker_ZoneMap.lua
@@ -18,7 +18,7 @@ end
 local L = DF.Language.GetLanguageTable(addonId)
 
 local _
-local GetQuestsForPlayerByMapID = C_TaskQuest.GetQuestsForPlayerByMapID or C_TaskQuest.GetQuestsOnMap
+local GetQuestsForPlayerByMapID = C_TaskQuest.GetQuestsOnMap
 local isWorldQuest = QuestUtils_IsQuestWorldQuest
 local GetNumQuestLogRewardCurrencies = WorldQuestTrackerAddon.GetNumQuestLogRewardCurrencies
 local GetQuestLogRewardInfo = GetQuestLogRewardInfo
